ede1bac74c1f360a50eedeffec40bcf0a15320ad
RedToastToy <74750946+RedToastToy@users.noreply.github.com>
2022-06-13 13:09:05 -0600

    Update FloatScript.cs

--- FloatScript.cs
+++ FloatScript.cs
@@ -1,1 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
+public class FloatScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float Mass;
+    private float Drag;
+    private float angularDrag;
+    public TextMeshProUGUI masstext;
+    public Slider massslider;
+    public TextMeshProUGUI dragtext;
+    public Slider dragslider;
+    public TextMeshProUGUI angulardragtext;
+    public Slider angulardragslider;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Mass = massslider.value;
+        masstext.text = "Mass: " + Mass.ToString();
+        rb.mass = Mass;
+        Drag = dragslider.value;
+        dragtext.text = "Drag: " + Drag.ToString();
+        rb.drag = Drag;
+        angularDrag = angulardragslider.value;
+        angulardragtext.text = "Angular Drag: " + angularDrag.ToString();
+        rb.angularDrag = angularDrag;
+    }
+}
